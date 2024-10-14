135aac6425bbd6d765511c48284881a1cb487fb6
DialloAbdourahman <dialliabdourahman78@gmail.com>
2024-10-14 14:48:17 +0100

    test

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,6 +1,26 @@
-import { Response, Request, NextFunction } from "express";
+// import { Response, Request, NextFunction } from "express";
+// import { CustomError } from "../errors/custom-error";
+// import { CODE } from "../enums/codes";
+
+// export const errorHandler = (
+//   err: Error,
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   if (err instanceof CustomError) {
+//     return res.status(err.statusCode).send(err.serializeErrors());
+//   }
+
+//   console.error("Unexpected error occured", err);
+//   res.status(500).send({
+//     code: CODE.UNEXPECTED_ERROR,
+//     message: "Something went wrong",
+//   });
+// };
+
+import { NextFunction, Request, Response } from "express";
 import { CustomError } from "../errors/custom-error";
-import { CODE } from "../enums/codes";
 
 export const errorHandler = (
   err: Error,
@@ -9,12 +29,17 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof CustomError) {
-    return res.status(err.statusCode).send(err.serializeErrors());
+    return res.status(err.statusCode).send({
+      errors: err.serializeErrors(),
+    });
   }
 
-  console.error("Unexpected error occured", err);
+  console.error("Unexpected error in middleware", err);
   res.status(500).send({
-    code: CODE.UNEXPECTED_ERROR,
-    message: "Something went wrong",
+    errors: [
+      {
+        message: "Something went wrong",
+      },
+    ],
   });
 };
