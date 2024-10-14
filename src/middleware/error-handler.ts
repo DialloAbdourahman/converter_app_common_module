e103bc04a0f370f10377482c309e45d6762ca9fe
DialloAbdourahman <dialliabdourahman78@gmail.com>
2024-10-14 13:43:22 +0100

    error middleware corrected

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -3,16 +3,16 @@ import { CustomError } from "../errors/custom-error";
 import { CODE } from "../enums/codes";
 
 export const errorHandler = (
-  error: Error,
+  err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (error instanceof CustomError) {
-    return res.status(error.statusCode).send(error.serializeErrors());
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send(err.serializeErrors());
   }
 
-  console.error("Unexpected error occured", error);
+  console.error("Unexpected error occured", err);
   res.status(500).send({
     code: CODE.UNEXPECTED_ERROR,
     message: "Something went wrong",
