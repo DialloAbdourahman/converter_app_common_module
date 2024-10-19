5d504634ceda7abf789e4771b8f299f4681fb812
DialloAbdourahman <dialliabdourahman78@gmail.com>
2024-10-19 11:52:41 +0100

    Multer error added

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -8,11 +8,23 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  // CUSTOM ERROR
   if (err instanceof CustomError) {
     res.status(err.statusCode).send(err.serializeErrors());
     return;
   }
 
+  // MULTER POTENTIAL ERROR
+  const multerPotentialError: any = err;
+  if (multerPotentialError?.code === "LIMIT_FILE_SIZE") {
+    res.status(400).send({
+      code: CODE.MULTER_SIZE_ERROR,
+      message: "File too large",
+    });
+    return;
+  }
+
+  // UNEXPECTED ERROR
   console.error("Unexpected error occured", err);
   res.status(500).send({
     code: CODE.UNEXPECTED_ERROR,
