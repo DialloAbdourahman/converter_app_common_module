71afa04e6ee7acbf15da2d8c0e63673588ef3455
DialloAbdourahman <dialliabdourahman78@gmail.com>
2024-10-21 14:10:01 +0100

    update to aws shelper

--- src/aws/aws-s3-helper.ts
+++ src/aws/aws-s3-helper.ts
@@ -4,6 +4,9 @@ import {
   S3Client,
 } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { PassThrough } from "stream";
+import { spawn } from "child_process";
+import { generateRandomString } from "../utils/generateRandomString";
 
 export class AwsS3Helper {
   private s3: S3Client;
@@ -40,7 +43,22 @@ export class AwsS3Helper {
 
     try {
       await this.s3.send(command);
-      // if(data.$metadata.httpStatusCode === 200)
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async uploadAudio(key: string, mp3Stream: PassThrough) {
+    const params = {
+      Bucket: this.bucketName,
+      Key: `videos/${key}`,
+      Body: mp3Stream,
+    };
+
+    const command = new PutObjectCommand(params);
+
+    try {
+      await this.s3.send(command);
     } catch (error) {
       throw error;
     }
@@ -58,16 +76,62 @@ export class AwsS3Helper {
     return url;
   }
 
-  // async getVideo(key: string) {
-  //   const params = {
-  //     Bucket: this.bucketName,
-  //     Key: `videos/${key}`,
-  //   };
+  async getVideoStreamFromS3(
+    key: string
+  ): Promise<NodeJS.ReadableStream | undefined> {
+    const params = {
+      Bucket: this.bucketName,
+      Key: `videos/${key}`,
+    };
 
-  //   const command = new GetObjectCommand(params);
+    const command = new GetObjectCommand(params);
 
-  //   const video = await this.s3.send(command);
-  // }
+    const video = await this.s3.send(command);
+
+    return video.Body as NodeJS.ReadableStream;
+  }
+
+  async convertVideoToMp3(key: string) {
+    try {
+      const videoStream = await this.getVideoStreamFromS3(key);
+
+      const mp3Stream = new PassThrough();
+
+      // Use `spawn` to convert the video stream to MP3 using ffmpeg
+      const ffmpegProcess = spawn("ffmpeg", [
+        "-i",
+        "pipe:0", // Input from stdin (video stream)
+        "-q:a",
+        "0", // Best audio quality
+        "-map",
+        "a", // Extract only audio
+        "-f",
+        "mp3", // Output format MP3
+        "pipe:1", // Output to stdout (so it can be piped)
+      ]);
+
+      videoStream?.pipe(ffmpegProcess.stdin);
+
+      ffmpegProcess.stdout.pipe(mp3Stream);
+
+      const s3key = generateRandomString(10);
+      await this.uploadAudio(s3key, mp3Stream);
+
+      ffmpegProcess.on("close", (code) => {
+        if (code === 0) {
+          console.log("ffmpeg finished successfully");
+        } else {
+          console.log(`ffmpeg process exited with code ${code}`);
+        }
+      });
+
+      ffmpegProcess.stderr.on("data", (data) => {
+        console.error(`ffmpeg error: ${data}`);
+      });
+    } catch (error) {
+      console.error("Error converting video to MP3:", error);
+    }
+  }
 
   async delete() {}
 }
